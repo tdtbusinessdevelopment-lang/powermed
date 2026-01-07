5fb3e22dccbcf3dc1dd21214e1e0d301413e8777
markcaram47 <111546363+markcaram47@users.noreply.github.com>
2026-01-07 09:45:10 +0800

    vyhv
    bj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import LandingPage from './pages/landingpage.jsx'
+import Products from './pages/products.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
       </div>
     </Router>
