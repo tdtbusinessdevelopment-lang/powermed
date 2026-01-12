5d2f0fde83c22d67b25eb21d548ebd5a8350e43e
ecofltm <cherishsarmiento20@gmail.com>
2026-01-12 14:03:50 +0800

    footer (no css)

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import '../styles/footer.css'
+
 
 export default function footer() {
   return (
-    <div>footer</div>
+    <div className="footer">
+        <div className="footer-content">
+            <div className="footer-first-section">
+                <div className="footer-img">
+                    <img src="https://figmage.com/images/iMa9rM-IVDGz6AHEzuP_s.png" alt="" />
+                </div>
+                <div className="footer-button">
+                    <button>Explore Powermed Products</button>
+                </div>
+            </div>
+            <div className="footer-second-section">
+                <div className="learn">
+                    <h3>LEARN</h3>
+                    <p>FAQs</p>
+                    <p>Menu</p>
+                    <p>Pricing</p>
+                    <p>My Account</p>
+                </div>
+            </div>
+            <div className="footer-third-section">
+                <div className="support">
+                    <h3>SUPPORT</h3>
+                    <p>Contact</p>
+                    <p>FAQ</p>
+                    <p>Partner with Us</p>
+                    <p>My Account</p>
+                </div>
+            </div>
+            <div className="footer-fourth-section">
+                <div className="send email">
+                    <h3>WELLNESS STARTS HERE.</h3>
+                    <p><span>Be the first to know.</span> Join the Nuthera community for menu drops, chef notes, and wellness tips tailored to your routine</p>
+                    <input className="email-input" type="email" placeholder="Enter your email" />
+                    <button className="email-button">Subscribe</button>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
